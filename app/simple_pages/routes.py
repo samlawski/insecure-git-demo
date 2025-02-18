6e9d8c4a37e7f88c92f8c103851cc6f655796056
Samuel Boguslawski <sam.bogu@gmail.com>
2025-02-18 16:06:20 +0100

    Create index HTML template

--- app/simple_pages/routes.py
+++ app/simple_pages/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 blueprint = Blueprint('simple_pages', __name__)
 
 @blueprint.get('/')
 def index():
-  return 'Hello World'
+  return render_template('simple_pages/index.html')
