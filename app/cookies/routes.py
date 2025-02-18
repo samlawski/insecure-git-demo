ac9a6d5c25e781d4d2ae628a75b34f8173a1b093
Samuel Boguslawski <sam.bogu@gmail.com>
2025-02-18 16:17:23 +0100

    Add page for individual cookies

--- app/cookies/routes.py
+++ app/cookies/routes.py
@@ -10,3 +10,11 @@ def cookies():
   cookies = response.json()
 
   return render_template('cookies/index.html', cookies=cookies)
+
+
+@blueprint.get('/cookies/<id>')
+def cookie(id):
+  response = requests.get(url=f'{API_URL}/api/cookies/{id}?key={API_KEY}')
+  cookie = response.json()
+
+  return render_template('cookies/show.html', cookie=cookie )
